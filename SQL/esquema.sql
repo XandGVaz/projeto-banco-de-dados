b9912094c04f98cb9b98fd75402dd1171318f391
XandGVaz <vitorgarciavaz@gmail.com>
2025-10-25 20:04:22 -0300

    feature: primeira versão do esquema do banco de dados

--- SQL/esquema.sql
+++ SQL/esquema.sql
@@ -41,7 +41,7 @@ CREATE TABLE IF NOT EXISTS inventario (
 	fator_kk	CHAR(1)		    	NOT NULL CHECK(fator_kk IN ('K', 'k')),
 	estoque		INT					NOT NULL CHECK(estoque >= 0),
 	CONSTRAINT  pk_inventario		PRIMARY KEY(local),
-	CONSTRAINT 	fk_inventario_local	FOREIGN KEY(local) REFERENCES(local) ON DELETE CASCADE			
+	CONSTRAINT 	fk_inventario_local	FOREIGN KEY(local) REFERENCES local(id) ON DELETE CASCADE			
 );
 
 CREATE TABLE IF NOT EXISTS nro_bolsas (
@@ -51,19 +51,19 @@ CREATE TABLE IF NOT EXISTS nro_bolsas (
 	fator_ee	CHAR(1)		    	NOT NULL CHECK(fator_ee IN ('E', 'e')),
 	fator_cc	CHAR(1)		    	NOT NULL CHECK(fator_cc IN ('C', 'c')),
 	fator_kk	CHAR(1)		    	NOT NULL CHECK(fator_kk IN ('K', 'k')),
-	quantidade	INT					NOT NULL CHECK(estoque >= 0),
+	quantidade	INT					NOT NULL CHECK(quantidade >= 0),
 	CONSTRAINT  pk_nro_bolsas		PRIMARY KEY(local),
-	CONSTRAINT 	fk_nro_bolsas_local	FOREIGN KEY(local) REFERENCES(local) ON DELETE CASCADE			
+	CONSTRAINT 	fk_nro_bolsas_local	FOREIGN KEY(local) REFERENCES local(id) ON DELETE CASCADE			
 );
 
 CREATE TABLE IF NOT EXISTS transferencia (
 	id				BIGSERIAL			NOT NULL,
 	local_origem	INT					NOT NULL,
 	local_destino	INT					NOT NULL,
-	data			TIMESTAMP			NOT NULL,
+	data			TIMESTAMP			NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	CONSTRAINT		pk_transferencia	PRIMARY KEY(id),
-	CONSTRAINT 		fk_local_origem		FOREIGN KEY(local) REFERENCES(local) ON DELETE CASCADE,
-	CONSTRAINT 		fk_local_destino	FOREIGN KEY(local) REFERENCES(local) ON DELETE CASCADE,
+	CONSTRAINT 		fk_local_origem		FOREIGN KEY(local_origem) REFERENCES local(id) ON DELETE CASCADE,
+	CONSTRAINT 		fk_local_destino	FOREIGN KEY(local_destino) REFERENCES local(id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS pessoa (
@@ -111,12 +111,65 @@ CREATE TABLE IF NOT EXISTS doacao (
 	id				BIGINT					NOT NULL,
 	doador			BIGINT					NOT NULL,
 	hemonucleo		INT						NOT NULL,
-	data			TIMESTAMP				NOT NULL,
+	data			TIMESTAMP				NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	profissional	BIGINT					NOT NULL,
 	CONSTRAINT 		pk_doacao				PRIMARY KEY(id),
 	CONSTRAINT		sk_doacao				UNIQUE(doador, hemonucleo, data), 
-	CONSTRAINT		fk_doacao_doador		FOREIGN KEY(doador) REFERENCES(doador) ON DELETE CASCADE,
-	CONSTRAINT		fk_doacao_hemonucleo	FOREIGN KEY(hemonucleo) REFERENCES(hemonucleo) ON DELETE CASCADE,
-	CONSTRAINT		fk_doacao_profissional	FOREIGN KEY(profissional) REFERENCES(profissional) ON DELETE CASCADE,
+	CONSTRAINT		fk_doacao_doador		FOREIGN KEY(doador) REFERENCES doador(id) ON DELETE CASCADE,
+	CONSTRAINT		fk_doacao_hemonucleo	FOREIGN KEY(hemonucleo) REFERENCES hemonucleo(id) ON DELETE CASCADE,
+	CONSTRAINT		fk_doacao_profissional	FOREIGN KEY(profissional) REFERENCES profissional(id) ON DELETE CASCADE
 );
 
+CREATE TABLE IF NOT EXISTS solicitacao (
+	id			BIGSERIAL					NOT NULL,
+	hospital	INT							NOT NULL,
+	receptor	BIGINT						NOT NULL,
+	data		TIMESTAMP					NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	CONSTRAINT	pk_solicitacao				PRIMARY KEY(id),
+	CONSTRAINT 	sk_solicitacao				UNIQUE(hospital, receptor, data),
+	CONSTRAINT	fk_solicitacao_hospital		FOREIGN KEY(hospital) REFERENCES hospital(id) ON DELETE CASCADE,
+	CONSTRAINT	fk_solicitacao_receptor		FOREIGN KEY(receptor) REFERENCES receptor(id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS socorrimento (
+	id			BIGSERIAL					NOT NULL,
+	ambulancia	INT							NOT NULL,
+	receptor	BIGINT						NOT NULL,
+	data		TIMESTAMP					NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	CONSTRAINT	pk_socorrimento				PRIMARY KEY(id),
+	CONSTRAINT 	sk_socorrimento				UNIQUE(ambulancia, receptor, data),
+	CONSTRAINT	fk_socorrimento_ambulancia	FOREIGN KEY(ambulancia) REFERENCES ambulancia(id) ON DELETE CASCADE,
+	CONSTRAINT	fk_socorrimento_receptor	FOREIGN KEY(receptor) REFERENCES receptor(id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS bolsa_de_sangue (
+	doacao			BIGINT				  NOT NULL,
+	numero			INT				  	  NOT NULL CHECK(numero >= 1 AND numero <= 4),
+	volueme_real	INT					  NOT NULL CHECK(volueme_real >= 1 AND volueme_real <= 450),
+	local_atual		INT					  NOT NULL,
+	fator_rh		CHAR(1)				  NOT NULL CHECK(fator_rh IN ('+','-')),
+	fator_abo		VARCHAR(2)			  NOT NULL CHECK(fator_abo IN ('AB', 'A', 'B', 'O')),
+	fator_ee		CHAR(1)		    	  NOT NULL CHECK(fator_ee IN ('E', 'e')),
+	fator_cc		CHAR(1)		    	  NOT NULL CHECK(fator_cc IN ('C', 'c')),
+	fator_kk		CHAR(1)		    	  NOT NULL CHECK(fator_kk IN ('K', 'k')),
+	solicitacao		BIGINT,				
+	socorrimento	BIGINT,
+	CONSTRAINT		pk_bolsa_de_sangue 	  PRIMARY KEY(doacao, numero),
+	CONSTRAINT		fk_bolsa_doacao		  FOREIGN KEY(doacao) REFERENCES doacao(id) ON DELETE CASCADE,
+	CONSTRAINT		fk_bolsa_solicitacao  FOREIGN KEY(solicitacao) REFERENCES solicitacao(id) ON DELETE CASCADE,
+	CONSTRAINT		fk_bolsa_socorrimento FOREIGN KEY(socorrimento) REFERENCES socorrimento(id) ON DELETE CASCADE
+);
+
+/*
+	TODO: fazer com que o atributo "numero" de "bolsa_de_sangue" seja gerado automaticamente para 
+	para uma mesma doação. Isso pode ser feito com um trigger
+*/
+
+CREATE TABLE IF NOT EXISTS envolve (
+	transferencia	BIGINT					 NOT NULL,
+	doacao			BIGINT					 NOT NULL,
+	numero			INT						 NOT NULL,
+	CONSTRAINT		pk_envolve				 PRIMARY KEY(transferencia, doacao, numero),
+	CONSTRAINT		fk_envolve_transferencia FOREIGN KEY(transferencia) REFERENCES transferencia(id) ON DELETE CASCADE,
+	CONSTRAINT		fk_envolve_bolsa		 FOREIGN KEY(doacao, numero) REFERENCES bolsa_de_sangue(doacao, numero) ON DELETE CASCADE
+);
