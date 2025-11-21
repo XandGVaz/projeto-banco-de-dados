6b2e8b0082a46010dd742862ebcd6da72f0f70fa
XandGVaz <vitorgarciavaz@gmail.com>
2025-11-21 19:37:23 -0300

    feat: criação script de inserção de dados

---
+++ SQL/dados.sql
@@ -0,0 +1,8 @@
+/*
+        Script SQL para inserimento de dados no banco de dados do sistema de doaçao de sangue (Grupo 06)
+        Autores:
+               - Danielle Pereira - 11918539
+               - Gabriel Dezejácomo Maruschi - 14571525
+               - Pedro Gasparelo Leme - 14602421
+               - Vitor Alexandre Garcia Vaz - 14611432
+*/
