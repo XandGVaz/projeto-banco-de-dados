b9ffa0278382076bd4f8194df3da77a21ad5b0cf
XandGVaz <vitorgarciavaz@gmail.com>
2025-11-20 21:47:31 -0300

    feat: criação do arquivo de consultas

---
+++ SQL/consultas.sql
@@ -0,0 +1,8 @@
+/*
+        Script SQL referente às consultas do sistema de doaçao de sangue (Grupo 06)
+        Autores:
+               - Danielle Pereira - 11918539
+               - Gabriel Dezejácomo Maruschi - 14571525
+               - Pedro Gasparelo Leme - 14602421
+               - Vitor Alexandre Garcia Vaz - 14611432
+*/
